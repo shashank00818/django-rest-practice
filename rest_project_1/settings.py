9438f05f64738d945f88f544fdaf843973128f33
shashank00818 <shashank00818@gmail.com>
2023-07-05 11:35:05 +0530

    added tags app

--- rest_project_1/settings.py
+++ rest_project_1/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'tags',
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
---
+++ tags/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+    slug = models.CharField(max_length=30, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+# Create your models here.
