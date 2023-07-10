f7877ca3c5c263a7a8b125077f7639a48dbf06bb
shashank00818 <shashank00818@gmail.com>
2023-07-10 21:43:41 +0530

    pagination and throttling

--- tags/views.py
+++ tags/views.py
@@ -2,8 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from tags.serializers import CreateTagSerializer, ReadTagSerializer
 from tags.models import Tag
-from rest_framework import status
+from rest_framework import status, throttling
 from django.utils.text import slugify
+from rest_framework.generics import ListAPIView, RetrieveAPIView, DestroyAPIView
+from tags.filters import StandardResultsSetPagination, CustomThrottle
 
 class CreateTag(APIView):
 
@@ -21,3 +23,41 @@ class CreateTag(APIView):
             return Response(response_data,status=status.HTTP_200_OK)
         else:
             return Response(create_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+# simple api view
+class ListTagV1(APIView):
+
+    def get(self, request):
+        queryset = Tag.objects.all()
+        response_data = ReadTagSerializer(queryset, many=Tag).data 
+        return Response(response_data, status=status.HTTP_200_OK)
+
+# list api view
+class ListTagV2(ListAPIView):
+    queryset = Tag.objects.all()
+    # _class means a single class 
+    serializer_class = ReadTagSerializer
+    # _class means a single class 
+    pagination_class = StandardResultsSetPagination
+    # _classes it means multiple classes / tuple of classes
+    throttle_classes = (CustomThrottle, )
+
+class TagDetailV1(APIView):
+
+    def get(self, request, slug):
+        try:
+            tag_object = Tag.objects.get(slug=slug)
+        except Tag.DoesNotExist:
+            error_response = {"error" : True, "message": "Tag does not exist"}
+            return Response(error_response, status=status.HTTP_400_BAD_REQUEST)
+        except Tag.MultipleObjectsReturned:
+            error_response = {"error" : True, "message": "Multiple tags exist with the same slug"}
+            return Response(error_response, status=status.HTTP_400_BAD_REQUEST)
+        response_data = ReadTagSerializer(instance=tag_object).data 
+        return Response(response_data, status=status.HTTP_200_OK)
+
+class TagDetailV2(RetrieveAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = ReadTagSerializer
+    lookup_field = "slug"
